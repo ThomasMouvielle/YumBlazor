333ca105b93b3bac0eef23b8d2366ae0d917d475
Thomas MOUVIELLE <tm@bms-engineering.lu>
2024-09-05 14:42:00 +0200

    NEW (master) Category Delete Modal

---
+++ wwwroot/js/common.js
@@ -0,0 +1,6 @@
+function ShowConfirmationModal() {
+    bootstrap.Modal.getOrCreateInstance('#bsConfirmationModal').show();
+}
+
+function HideConfirmationModal() {
+    bootstrap.Modal.getOrCreateInstance('#bsConfirmationModal').hide();}
